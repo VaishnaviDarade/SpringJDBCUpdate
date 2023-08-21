4b775eed4e94fb4bbea6b32b98edce8cb7f1768e
YUVA <tech.vaishnavid@gmail.com>
2023-08-21 16:21:36 +0530

    added update values

---
+++ src/main/java/com/mahagan/dao/ProjectDao.java
@@ -0,0 +1,29 @@
+package com.mahagan.dao;
+
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.object.SqlUpdate;
+
+import javax.sql.DataSource;
+import java.sql.Types;
+
+public class ProjectDao {
+    private DataSource dataSource;
+    private ProjectSqlUpdate projectSqlUpdate;
+    private final String SQL_FOR_PROJECT="Update project set title=? where projectNo=?";
+
+    public ProjectDao(DataSource dataSource){
+        this.dataSource=dataSource;
+        projectSqlUpdate=new ProjectSqlUpdate(dataSource,SQL_FOR_PROJECT);
+    }
+    public int UpdateProject(int projectNo,String title){
+      return projectSqlUpdate.update(new Object[]{title,projectNo} );
+    }
+    private final class ProjectSqlUpdate extends SqlUpdate{
+        public ProjectSqlUpdate(DataSource dataSource,String sqlQuery){
+            super(dataSource,sqlQuery);
+            declareParameter(new SqlParameter(java.sql.Types.VARCHAR));
+            declareParameter(new SqlParameter(java.sql.Types.INTEGER));
+            compile();
+        }
+    }
+}
